f8e1aa5b09c1223b88486710278fe1f99e1e1d93
mattius9 <matthew.krasucki@gmail.com>
2021-06-11 13:15:43 -0400

    create pages, components dirs, App in pages, create AuthPage

---
+++ cleanr/src/pages/App/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import AuthPage from '../AuthPage/AuthPage';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>cleanR</h1>
+      <h2>Welcome to cleanR</h2>
+
+      <AuthPage />
+    </div>
+  );
+}
+
+export default App;
