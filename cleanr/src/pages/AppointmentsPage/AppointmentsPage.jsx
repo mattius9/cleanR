14144576347d4999e08938e6b66349bd00e3379c
mattius9 <matthew.krasucki@gmail.com>
2021-06-14 17:53:29 -0400

    reverse address lookup fix

--- cleanr/src/pages/AppointmentsPage/AppointmentsPage.jsx
+++ cleanr/src/pages/AppointmentsPage/AppointmentsPage.jsx
@@ -1,6 +1,5 @@
 import './AppointmentsPage.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
 import AppointmentList from '../../components/AppointmentList/AppointmentList';
 
 export default function AppointmentsPage() {
--- cleanr/src/pages/MapPage/MapPage.jsx
+++ cleanr/src/pages/MapPage/MapPage.jsx
@@ -1,43 +1,58 @@
 import './MapPage.css';
 import React, { useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
-import { MapContainer, TileLayer, Marker, Popup, useMap} from 'react-leaflet';
-import { getCurrentLatLng } from '../../utilities/geolocation';
+import { MapContainer, Marker, Popup, useMap} from 'react-leaflet';
+import { BasemapLayer} from "react-esri-leaflet";
 
 export default function MapPage() {
-    
+
+    // Lat & LNG used for testing
     const [lat, setLat] = useState(51.505);
     const [lng, setLng] = useState(-0.09);
 
+    async function addressFetch(lat,lng){
+        let response = await fetch(`https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?f=json&featureTypes=PointAddress&location=${lng}%2C${lat}`);
+        response = await response.json();
+        console.log(response.address);
+        console.log(response.address.Address);
+    }
+
+    // function addMarker(){
+    //     e.preventDefault();
+    //     const marker = e.latlng;
+    //     setMarkers(currentMarkers => [...currentMarkers, marker])
+    //     console.log(markers);
+    //   };
+
     function LocationMarker() {
         const [position, setPosition] = useState(null)
-        
         const map = useMap();
-
+        
         useEffect(() => {
             map.locate().on("locationfound", function(e){
-                setPosition(e.latlng)
-                map.panTo(e.latlng)
+                setPosition(e.latlng);
+                map.panTo(e.latlng);
+                console.log("done");
+                addressFetch(e.latlng.lat, e.latlng.lng);
             });
         }, [map]);
-      
+        
         return position === null ? null : (
-          <Marker position={position}>
+            <Marker position={position}>
             <Popup>You are here</Popup>
-          </Marker>
+            </Marker>
         )
-      }
+    }
 
     return (
         <div className="Page">
             Map Page
             <MapContainer center={[lat, lng]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
+
+                <BasemapLayer name="Streets" />
                 <LocationMarker />
             </MapContainer>
+
+            <button onClick={()=> addressFetch(lat, lng)}>Get Address</button>
         </div>
     )
 }
