7a9da77663468de51fa16bf34891123ec33bc092
Ryan Joseph <ryankjoseph@gmail.com>
2021-06-15 19:18:40 -0400

    Implement toggle function

--- cleanr/src/pages/App/App.js
+++ cleanr/src/pages/App/App.js
@@ -8,11 +8,42 @@ import ServicesPage from '../ServicesPage/ServicesPage';
 import { getUser } from '../../utilities/users-service';
 
 function App() {
+  function getRole(role){
+    if (user.roles){
+      if (user.roles.length === 1){
+        return user.roles[0].role
+      }
+      else{
+        setToggleView(true)
+        return user.roles[0].role
+      }
+    }
+    else{
+      return null;
+    }
+  }
   const [user, setUser] = useState(getUser());
+  const [toggleView, setToggleView] = useState(false)
+  const [currentRole, setCurrentRole] = useState({role:getRole()})
+
+
+  // const [currentRole, setCurrentRole] =useState(null);
+
+  // { currentRole ? null:((user.roles.length>1) ? <div>
+  //   TOGGLE THE VIEW
+  // </div>:setCurrentRole(user.roles[0].role)) }
 
   return (
+    
     <div className="App">
       <header className="App-header">cleanR</header>
+      {!toggleView ? <div>
+        <button value="agent" className="toggle-role" type="button" onClick={(e)=>{setCurrentRole({role: e.target.value})}}>Agent</button>
+        <button value="client" className="toggle-role" type="button" onClick={(e)=>{setCurrentRole({ role: e.target.value})}}>Client</button>
+        </div>:null}
+          <div>{currentRole.role}</div>
+          {/* <div>{user}</div> */}
+          {/* <div>{user.roles[0].role}</div> */}
       { user ? 
       <Switch>
         <Route path="/appointments">
