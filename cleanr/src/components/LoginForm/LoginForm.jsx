adc00c8d7bbe16b8445524f6eaa1d022667a4bb4
Ryan Joseph <ryankjoseph@gmail.com>
2021-06-15 23:26:42 -0400

    import login from user services

--- cleanr/src/components/LoginForm/LoginForm.jsx
+++ cleanr/src/components/LoginForm/LoginForm.jsx
@@ -1,22 +1,34 @@
 
-import React from 'react'
-function testThis(props){
-    console.log('dootdoot')
-}
+import React, {useState} from 'react'
+import {login} from "../../utilities/users-service"
 export default function LoginForm(props) {
+    async function submitLogin(){
+        console.log(loginName)
+        console.log(loginPass)
+        let credentials = {username: loginName, password: loginPass}
+        try{
+
+            const user = await login(credentials);
+        }
+        catch{
+            console.log("NO WORKING!")
+        }
+    }
+    const [loginName, setLoginName] = useState('')
+    const [loginPass, setLoginPass] = useState('')
     return (
         <div className="Component">
             Login
 
             <form>
                 <label for="username">Username:</label>
-                <input id="username"></input>
+                <input value={loginName} onChange={(e)=>{setLoginName(e.target.value)}} type="text" id="username"></input>
                 
                 <label for="password"> Password</label>
-                <input type="password" id="password"></input>
-                <input type="submit" value="Enter!" />
+                <input value={loginPass} onChange={(e)=>{setLoginPass(e.target.value)}} type="password" id="password"></input>
+                <button type="button" onClick={()=>{submitLogin()}}>Login</button>
             </form>
-            <button onClick={()=>{testThis()}}>TEST</button>
+            <button onClick={()=>{submitLogin()}}>TEST</button>
         </div>
     )
 }
