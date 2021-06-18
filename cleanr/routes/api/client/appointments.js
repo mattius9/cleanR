936a0dab8de3c523c3518ef572f0d12774189f64
mattius9 <matthew.krasucki@gmail.com>
2021-06-17 23:17:56 -0400

    renaming routes

--- cleanr/routes/api/client/appointments.js
+++ cleanr/routes/api/client/appointments.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const clientAppointmentsCtrl = require('../../../controllers/client/appointments');
+const appointmentsCtrl = require('../../../controllers/client/appointments');
 const router = express.Router();
 
 // POST /api/client/appointments (retrieves the client's appointment info) for calendar/list view
-router.get('/client/index', clientAppointmentsCtrl.index);
+router.get('/client/index', appointmentsCtrl.index);
 // POST /api/client/create (allows the client to create an appointment, awaiting status) 
-router.post('/client/create', clientAppointmentsCtrl.create);
+router.post('/client/create', appointmentsCtrl.create);
 //findById and update
-router.put('/client/respond/:id', agentAppointmentsCtrl.respond);
+// router.put('/client/respond/:id', appointmentsCtrl.respond);
 module.exports = router;
